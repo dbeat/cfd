221892e760593d95c36e222aa2d9f3dfff4984da
francois <frns.roy@gmail.com>
2019-12-14 13:11:23 -0500

    Added fenics

---
+++ pack.py
@@ -0,0 +1,24 @@
+import dolfin
+import click
+import sympy
+import numpy as np
+import scipy.constants
+import cppimport
+import sphinx
+import PyQt5
+import pint
+import unipath
+import pygmsh
+import meshio
+import mshr
+import vtk
+
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+    main()
